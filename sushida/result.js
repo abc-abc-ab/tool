1c194147df32d911e28a49371e386afd15166d20
Marui <152158429+abc-abc-ab@users.noreply.github.com>
2025-09-10 19:35:02 +0900

    Initial Commit

--- sushida/result.js
+++ sushida/result.js
@@ -7,3 +7,6 @@ for (const [k, v] in params){
 }
 console.groupEnd("URLSearchParams")
 
+const [course, difficulty, profit, average, miss, ranking] = 
+      [Number(params.get("course")),Number(params.get("difficulty")),Number(params.get("profit")),
+       Number(params.get("average")),Number(params.get("miss")),10001 - Number(params.get("ranking"))];
