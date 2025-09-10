d818e57222a02f148e2158c47df47e51ee25f8e8
Marui <152158429+abc-abc-ab@users.noreply.github.com>
2025-09-10 19:27:22 +0900

    Initial Commit

--- sushida/result.js
+++ sushida/result.js
@@ -2,10 +2,8 @@ const params = new URLSearchParams(location.search);
 
 
 console.groupCollapsed("URLSearchParams")
-for ([k, v] in params){
+for (const [k, v] in params){
     console.log(`${k}: ${v}`)
 }
 console.groupEnd("URLSearchParams")
 
-
-document.createElement("a")
