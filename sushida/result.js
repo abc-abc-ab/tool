b8f9b3a93706b2841d7a15f7499532ffd68201ba
Marui <152158429+abc-abc-ab@users.noreply.github.com>
2025-09-13 11:33:17 +0900

    Initial Commit

--- sushida/result.js
+++ sushida/result.js
@@ -1,7 +1,7 @@
 const params = new URLSearchParams(location.search),
 resultElm = document.getElementById("result");
 
-
+if (!params.has("course")) { history.replaceState(null, "", "./index.html") }
 console.groupCollapsed("URLSearchParams")
 for (let [key, value] of params.entries()){
     console.log(`${key}: ${value}`)
@@ -9,13 +9,24 @@ for (let [key, value] of params.entries()){
 console.groupEnd("URLSearchParams")
 
 const [course, difficulty, profit, average, miss, ranking] = 
-      [Number(params.get("course")),Number(params.get("difficulty")),Number(params.get("profit")),
-       Number(params.get("average")),Number(params.get("miss")),10001 - Number(params.get("ranking"))];
+      [Number(params.get("course")), Number(params.get("difficulty")), Number(params.get("profit")),
+       Number(params.get("average")), Number(params.get("miss")), 10001 - Number(params.get("ranking"))];
 
 const score = course * difficulty * Math.abs( (average - miss) + ranking + profit);
-if (!isNaN(score)){
+if (!IsInvalid){
     history.replaceState(null, "", "./result.html")
     resultElm.insertAdjacentText("beforeend", score.toLocaleString());
 }else{
     document.location.href = "./index.html?error=NaN";
+}
+
+function IsInvalid(){
+    let result = false;
+    if (0 > (  100 * course  + profit )) {result = true;}
+    else if (profit % 10) {result = true;}
+    else if (10002 < ranking || 1 > ranking) {result = true;}
+    else if (!/30|50|100/.test(course)) {result = true;}
+    else if (!/1|2|3|4|7/.test(difficulty)) {result = true;}
+    else if (0 >= score)
+    return result;
 }
