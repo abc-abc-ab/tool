de0a8f80e1b4f9b6932b89726400a402e5985e34
Marui <152158429+abc-abc-ab@users.noreply.github.com>
2025-09-13 14:38:18 +0900

    Initial Commit

--- sushida/result.js
+++ sushida/result.js
@@ -1,5 +1,6 @@
 const params = new URLSearchParams(location.search), //JS
-resultElm = document.getElementById("result");
+resultElm = document.getElementById("result"),
+outputElm = document.getElementById("output");
 
 if (!params.has("course")) { history.replaceState(null, "", "./index.html") }
 console.groupCollapsed("URLSearchParams")
@@ -10,14 +11,26 @@ console.groupEnd("URLSearchParams")
 
 const [course, difficulty, profit, average, miss, ranking] = 
       [Number(params.get("course")), Number(params.get("difficulty")), Number(params.get("profit")),
-       Number(params.get("average")), Number(params.get("miss")), 10001 - Number(params.get("ranking"))];
+       Number(params.get("average"))*60, Number(params.get("miss")), 10001 - Number(params.get("ranking"))];
 
 const score = course * difficulty * Math.abs( (average - miss) + ranking + profit);
-if (!IsInvalid()){
+if (IsInvalid()) { document.location.href = "./index.html?error"; }
+{
     history.replaceState(null, "", "./result.html")
+    outputElm.insertAdjacentHTML("afterbegin",
+        `コース: ${course==30?"お手軽3,000":(course==50?"お勧め5,000":"高級10,000")}円コース<br>
+        難易度: ${["練習", "普通", "正確重視", "速度必須", null, null, "一発勝負"][difficulty - 1]}<br>
+        損益: ${profit == 0?"プラスマイナス":""}${Math.abs(profit)}円${0 < profit?"得":(0 > profit?"損":"")}<br>
+        平均タイプ数: ${average/60}回/秒 ／ ${average}回/分<br>
+        ミスタイプ数: ${miss}回<br>
+        ランキング: ${ranking == 10002?"表示なし":10001 - ranking}<br>
+        i コース: (i / 100) / ii 難易度: 練習1 普通2 正確重視3 速度必須4 一発勝負7 /<br>
+	    iii 損益: 損-iii 得+iii / iv 平均タイプ数: iv*60 (回/分) / v ミス数: そのまま /<br>
+	    vi 順位: 損-1 得10,001 - vi ( | ~ | は絶対値 )<br>
+        式) ${course}(i) * ${difficulty}(ii) * | (${average}(iv) - ${miss}(v)) + ${ranking}(vi) + ${profit}(iii) |`
+        
+    )
     resultElm.insertAdjacentText("beforeend", score.toLocaleString());
-}else{
-    window.setTimeout(()=>{document.location.href = "./index.html?error";}, 3000)
 }
 
 function IsInvalid(){
